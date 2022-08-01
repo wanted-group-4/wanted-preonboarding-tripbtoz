5d9e3969e2e05be2bc84e40301a22cf6b503e295
mirim <alfla6528@naver.com>
2022-08-01 16:30:12 +0900

    feat: router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/GlobalStyle';
+import Router from '@routes/Router';
 
 function App() {
-  return <div>helloworld!</div>;
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
