f8b382ea2fc257bf9345a74b5ad04f353391058f
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-08-02 18:19:31 +0900

    feat: 호텔 리스트 get api, 호텔 하나 get api (#11)
    * chore: react-query v4 설치
    
    * feat: react-query 기본 셋팅 및 api 폴더 생성
    
    * feat: 호텔 불러오기 api 작성 (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
 import Router from '@routes/Router';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router />
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Router />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
