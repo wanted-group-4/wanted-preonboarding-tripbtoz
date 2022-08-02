c661e63ccb2b18a45cfb39df457713df7252ce6b
park_so_young <soyoung931014@gmail.com>
2022-08-02 15:42:17 +0900

    design: CSS

--- src/components/modal/GuestReservation.tsx
+++ src/components/modal/GuestReservation.tsx
@@ -32,16 +32,68 @@ export default function GuestReservation() {
     </>
   );
 }
-const Container = styled.div``;
-const SectionBox = styled.div``;
-const FirstSection = styled.div``;
+const Container = styled.div`
+  width: 319px;
+  height: 190px;
+  border-radius: 5px;
+  background: #ffffff;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.25);
+`;
+const SectionBox = styled.div`
+  position: relative;
+  padding: 17px;
+`;
+const FirstSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 55px;
+`;
 const AgeGroup = styled.div``;
-const Age = styled.div``;
-const CountBox = styled.div``;
-const Count = styled.button``;
-const Number = styled.span``;
+const Age = styled.div`
+  color: ${({ theme }) => theme.color.grey_03};
+  position: relative;
+  left: -35px;
+  font-size: 12px;
+`;
+const CountBox = styled.div`
+  border: 1px solid #d9d9d9;
+  border-radius: 5px;
+`;
+const Count = styled.button`
+  color: ${({ theme }) => theme.color.pink_02};
+  padding: 5px;
+  width: 30px;
+  font-size: 20px;
+  font-weight: 500;
+`;
+const Number = styled.span`
+  display: inline-block;
+  width: 50px;
+  text-align: center;
+`;
 
-const SecondSection = styled.div``;
+const SecondSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 55px;
+`;
 
-const ThirdSection = styled.div``;
-const Button = styled.button<{ Apply?: any }>``;
+const ThirdSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+`;
+const Button = styled.button<{ Apply?: any }>`
+  width: 140px;
+  height: 35px;
+  background: ${props => (props.Apply ? '#FF375C' : '#FFFFFF')};
+  border: 1px solid #d9d9d9;
+  border-radius: 5px;
+  font-weight: 700;
+  font-size: 13px;
+  color: ${props => (props.Apply ? '#FFFFFF' : '#000000')};
+  margin-left: ${props => (props.Apply ? '10px' : '')};
+`;
