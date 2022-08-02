ee982bc9300f988154cd6b0e3a18423a454bcccd
park_so_young <soyoung931014@gmail.com>
2022-08-02 15:44:39 +0900

    design: CSS

--- src/components/modal/ConfirmReservation.tsx
+++ src/components/modal/ConfirmReservation.tsx
@@ -15,8 +15,45 @@ export default function ConfirmReservation() {
     </>
   );
 }
-const BackGround = styled.div``;
-const Container = styled.div``;
-const Text = styled.div``;
+const BackGround = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(238, 238, 238, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Container = styled.div`
+  width: 300px;
+  height: 200px;
+  background: #ffffff;
+  box-shadow: 0px 4px 15px 3px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.div`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 19px;
+  color: #000000;
+  position: relative;
+  top: -10px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  background: ${({ theme }) => theme.color.pink_02};
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 500;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  padding: 8px 35px;
+  position: relative;
+  right: -19px;
+  top: 30px;
+`;
