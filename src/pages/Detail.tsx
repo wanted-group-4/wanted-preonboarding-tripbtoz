8b1944d703a3f188009562128622020263d5e30f
Yeolha Sung <tjddufgk@gmail.com>
2022-08-02 14:07:17 +0900

    feat: Home, Detail 페이지 Hotel Card 컴포넌트 추가 (#4)
    * feat: images 추가
    
    * feat: Home, Detail 페이지 HotelCard 컴포넌트 추가

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import HotelCard from '@components/hotel/HotelCard';
 
+const DetailImageSize: { width: number; height: number } = {
+  width: 260,
+  height: 130,
+};
 function Detail() {
-  return <div>Detail</div>;
+  return (
+    <div>
+      <HotelCard
+        name="웨스틴 조선 서울 (Detail 카드)"
+        base={2}
+        max={4}
+        imageSize={DetailImageSize}
+      />
+    </div>
+  );
 }
 
 export default Detail;
