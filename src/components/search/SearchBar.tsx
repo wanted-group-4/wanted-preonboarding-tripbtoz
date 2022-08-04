8ed9664aa04c4d705034d38192f06ccdfea705f5
Sohui Suh <greenish0902@gmail.com>
2022-08-04 18:11:42 +0900

    feat: Search Bar 반응형 UI 구현 (#24)
    * chore: wrappers 폴더 절대경로 생성
    
    * feat: 화면 width, height를 받아오는 훅
    
    * fix: 컴포넌트 삭제
    
    * feat: 훅을 사용해 서치바 반응형 UI 구현

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import SearchDate from '@components/search/SearchDate';
 import SearchCount from '@components/search/SearchCount';
+import useWindowDimensions from '@hooks/useWindowDimensions';
+import theme from '@styles/theme';
 
 function SearchBar() {
+  const { width } = useWindowDimensions();
+  const [isWebWidth, setIsWebWidth] = useState(true);
+
+  useEffect(() => {
+    if (width <= +theme.size.tablet.slice(0, -2)) {
+      setIsWebWidth(false);
+    } else setIsWebWidth(true);
+  }, [width]);
+
   return (
     <SearchBarContainer>
-      <SearchDate />
-      {/* modal */}
-      <SearchCount />
-      {/* modal */}
+      <SearchDate isWeb={isWebWidth} />
+      <SearchCount isWeb={isWebWidth} />
     </SearchBarContainer>
   );
 }
@@ -18,8 +27,19 @@ function SearchBar() {
 export default SearchBar;
 
 const SearchBarContainer = styled.div`
+  margin: auto;
   display: flex;
   align-items: center;
   border: 1px solid ${({ theme }) => theme.color.grey_03};
   border-radius: 4px;
+  @media ${({ theme }) => theme.deviceSize.tablet} {
+    width: 100%;
+    display: block;
+    border: none;
+  }
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    min-width: 300px; // temp
+    display: block;
+    border: none;
+  }
 `;
--- src/components/search/SearchDate.tsx
+++ src/components/search/SearchDate.tsx
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 import { format, differenceInDays } from 'date-fns';
 import { AiOutlineCalendar } from 'react-icons/ai';
 
-import LabelInputWrapper from '@components/wrappers/LabelInputWrapper';
 import IconWrapper from '@components/wrappers/IconWrapper';
 import BarWrapper from '@components/wrappers/BarWrapper';
 import KeyWordContainer from '@src/components/common/KeyWordContainer';
-import { IStyledProps } from '@type/style';
+import { ISearchInnerProps } from '@type/search';
 import { CalendarModal } from '@components/calendar';
 
-function SearchDate() {
+function SearchDate({ isWeb }: ISearchInnerProps) {
   const [display, setDisplay] = useState(false);
   const [checkin, setCheckin] = useState('8월 1일'); // temp
   const [checkout, setCheckout] = useState('8월 2일'); // temp
@@ -53,11 +52,17 @@ function SearchDate() {
     <SearchDateContainer onClick={openCalendar}>
       <BarWrapper height="64px">
         <IconWrapper icon={<AiOutlineCalendar />} />
-        <LabelInputWrapper width="32%" label="체크인" value={checkin} />
+        <KeyWordContainer content="체크인 :" color="grey_03" />
+        <KeyWordContainer content={checkin} />
         <KeyWordContainer content="|" color="grey_03" />
-        <KeyWordContainer content={`${diff}박`} />
-        <KeyWordContainer color="grey_03" content="|" />
-        <LabelInputWrapper width="36%" label="체크아웃" value={checkout} />
+        {isWeb && (
+          <>
+            <KeyWordContainer content={`${diff}박`} />
+            <KeyWordContainer color="grey_03" content="|" />
+          </>
+        )}
+        <KeyWordContainer content="체크아웃 :" color="grey_03" />
+        <KeyWordContainer content={checkout} />
         <CalendarModal display={display} />
       </BarWrapper>
     </SearchDateContainer>
@@ -69,17 +74,16 @@ export default SearchDate;
 const SearchDateContainer = styled.div`
   cursor: pointer;
   padding: 0 16px;
-  width: 50%;
-  position: relative;
+  width: 60%;
   border-right: 1px solid ${({ theme }) => theme.color.grey_03};
-`;
-
-const TempCalender = styled.div<IStyledProps>`
-  margin-top: 12px;
-  width: ${({ theme }) => theme.size.tablet}; // 수정
-  height: 400px;
-  position: absolute;
-  top: ${props => props?.height};
-  left: 0;
-  background-color: ${({ theme }) => theme.color.grey_01};
+  @media ${({ theme }) => theme.deviceSize.tablet} {
+    margin-bottom: 8px;
+    padding: 0 48px;
+    width: 100%;
+    border: 1px solid ${({ theme }) => theme.color.grey_03};
+    border-radius: 4px;
+  }
+  @media ${({ theme }) => theme.deviceSize.mobile} {
+    padding: 0 12px;
+  }
 `;
