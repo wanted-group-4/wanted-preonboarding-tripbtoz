dffcc78d12f85c6bf94f29388aea8b3e4eb221aa
Yeolha Sung <tjddufgk@gmail.com>
2022-08-05 11:04:11 +0900

    design: 레이아웃 padding 네비게이션으로 이동

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -19,7 +19,6 @@ export default Layout;
 
 const LayoutContainer = styled.div`
   width: 900px;
-  padding: 25px 30px;
   margin: 0 auto;
   @media ${({ theme }) => theme.deviceSize.tablet} {
     width: 100%;
