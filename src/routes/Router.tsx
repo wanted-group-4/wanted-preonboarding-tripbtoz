35771140ee39ad188cb9e99f910ec7bccf10afd9
park_so_young <soyoung931014@gmail.com>
2022-08-06 02:13:57 +0900

    refactor: 라우터 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,13 +5,12 @@ import Layout from '@components/layout/Layout';
 import Search from '@pages/Search';
 import Detail from '@pages/Detail';
 import MyPage from '@pages/MyPage';
-import ConfirmReservation from '@src/components/modal/ConfirmReservation';
 
 function Router() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<ConfirmReservation />} />
+        <Route path="/" element={<Search />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/my_page" element={<MyPage />} />
       </Route>
