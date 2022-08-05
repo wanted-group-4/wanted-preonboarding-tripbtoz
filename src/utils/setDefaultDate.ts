db081cc1e40f6c4ac7e30d811df774ed4e735f86
Mirim Lee <mrlee3233@gmail.com>
2022-08-05 21:58:45 +0900

    feat: handleSearch 함수 적용 및 width변동에 따른 scorll 제어 (#60)
    * feat: submit fucntion 적용
    
    * design: css fixed 적용
    
    * refactor: defaultCheckDate location hook으로 이동
    
    * design: hover시 border 추가
    
    * feat: width 변경시 스크롤 제어 추가 & submit 실행시 modal false

--- src/utils/setDefaultDate.ts
+++ src/utils/setDefaultDate.ts
@@ -1,11 +1,9 @@
-import { ISearchData } from '@type/search';
 import { add, format } from 'date-fns';
 
 const setDefaultDate = (
   dateRef: React.MutableRefObject<{
     [key: string]: HTMLDivElement | null;
   }>,
-  searchData: ISearchData,
 ) => {
   const date = new Date();
 
@@ -20,9 +18,6 @@ const setDefaultDate = (
   dateRef.current[today]?.classList.add('today');
   dateRef.current[start]?.classList.add('start');
   dateRef.current[end]?.classList.add('end');
-
-  searchData.calendar.checkIn = format(add(date, { days: 7 }), 'yyyy-MM-dd');
-  searchData.calendar.checkOut = format(add(date, { days: 8 }), 'yyyy-MM-dd');
 };
 
 export default setDefaultDate;
