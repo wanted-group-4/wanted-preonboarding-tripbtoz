307359f14f57cea595ccaf75ce2e140596bb8782
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-08-05 22:00:10 +0900

    refactor : 호텔 get api 리팩토링 (#58)
    * refactor: fetchFilterHotels 에 try-catch 추가
    * refactor: 호텔 get api refactoring (#57)

--- src/api/searchApi.ts
+++ src/api/searchApi.ts
@@ -3,18 +3,26 @@ import { useQuery } from '@tanstack/react-query';
 
 import { BASE_URL } from '@api/index';
 
+const getDelay = (delay: number) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
 // 호텔 10개+@ 갖고오기
-export const fetchHotels = async ({ pageParam = 1 }) => {
+export const fetchHotels = async ({ pageParam = 1, queryKey }) => {
+  const maxPerson = queryKey[1];
   try {
-    const response = await axios.get(
-      `${BASE_URL}/hotels?_page=${pageParam}&_limit=10`,
-    );
-
-    const getDelay = (delay: number) =>
-      new Promise(resolve => setTimeout(resolve, delay));
-    await getDelay(500);
-
-    return response.data;
+    if (maxPerson) {
+      const response = await axios.get(
+        `${BASE_URL}/hotels?occupancy.max_gte=${maxPerson}&_sort=occupancy.max&_page=${pageParam}`,
+      );
+      await getDelay(500);
+      return response.data;
+    } else {
+      const response = await axios.get(
+        `${BASE_URL}/hotels?_page=${pageParam}&_limit=10`,
+      );
+      await getDelay(500);
+      return response.data;
+    }
   } catch (error) {
     console.log(error);
   }
@@ -35,14 +43,4 @@ const fetchHotelInformation = async ({ queryKey }) => {
 
 export const getHotelInformation = (hotelName: string) => {
   return useQuery(['hotel', 'detail', hotelName], fetchHotelInformation);
-};
-
-// 검색 시 max 기준으로 나열
-export const fetchFilterHotels = async ({ queryKey }) => {
-  const maxPerson = queryKey[2];
-  const pageParam = queryKey[3];
-  const response = await axios.get(
-    `${BASE_URL}/hotels?occupancy.max_gte=${maxPerson}&_sort=occupancy.max&_page=${pageParam}`,
-  );
-  return response.data;
 };
