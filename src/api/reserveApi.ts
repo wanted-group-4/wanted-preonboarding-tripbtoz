fdb441a841f041da73a285dda76b9de170d94d9b
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-08-05 18:09:36 +0900

    feat: 예약된 호텔 get api (#51)
    * refactore: patchReserveHotel에 try-catch 추가하기
    * feat: 예약 호텔 리스트를 갖고오는 get api 작성 (#50)
    * refactor: child에서 kid로 수정

--- src/api/reserveApi.ts
+++ src/api/reserveApi.ts
@@ -13,18 +13,33 @@ export const patchReserveHotel = () => {
     name: string;
     check_in: any;
     check_out: any;
-    occupancy: { adult: number; child: number };
+    occupancy: { adult: number; kid: number };
   }) => {
-    await axios.post(`${BASE_URL}/reserved`, {
-      name,
-      check_in,
-      check_out,
-      occupancy,
-    });
+    try {
+      await axios.post(`${BASE_URL}/reserved`, {
+        name,
+        check_in,
+        check_out,
+        occupancy,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const queryClient = useQueryClient();
   return useMutation(fetch, {
     onSuccess: () => queryClient.invalidateQueries(['reserved']),
   });
+};
+
+export const fetchReserveHotels = async ({ pageParam = 1 }) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/reserved?_page=${pageParam}&_limit=10`,
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
